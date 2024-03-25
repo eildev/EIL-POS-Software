16040f445295e8fa66d0dc7cfc9ca008054ac171
kishormahmud9 <topfazil999@gmail.com>
2024-03-25 14:23:05 +0600

    Category CRUD is Ongoing

--- resources/views/body/sidebar.blade.php
+++ resources/views/body/sidebar.blade.php
@@ -19,30 +19,9 @@
                 </a>
             </li>
             <li class="nav-item nav-category">Products</li>
-            {{-- <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#emails" role="button" aria-expanded="false"
-                    aria-controls="emails">
-                    <i class="link-icon" data-feather="mail"></i>
-                    <span class="link-title">Category</span>
-                    <i class="link-arrow" data-feather="chevron-down"></i>
-                </a>
-                <div class="collapse" id="emails">
-                    <ul class="nav sub-menu">
-                        <li class="nav-item">
-                            <a href="pages/email/inbox.html" class="nav-link">Inbox</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/email/read.html" class="nav-link">Read</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="pages/email/compose.html" class="nav-link">Compose</a>
-                        </li>
-                    </ul>
-                </div>
-            </li> --}}
             <li class="nav-item">
                 <a href="{{ route('category') }}" class="nav-link">
-                    <i class="link-icon" data-feather="book"></i>
+                    <i class="link-icon" data-feather="box"></i>
                     <span class="link-title">Category</span>
                 </a>
             </li>
@@ -63,13 +42,13 @@
                 <div class="collapse" id="general-pages">
                     <ul class="nav sub-menu">
                         <li class="nav-item">
-                            <a href="{{route('branch')}}" class="nav-link">Add Branch </a>
+                            <a href="{{ route('branch') }}" class="nav-link">Add Branch </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{route('branch.view')}}" class="nav-link">Manage Branch</a>
+                            <a href="{{ route('branch.view') }}" class="nav-link">Manage Branch</a>
                         </li>
-                       
-                     
+
+
                     </ul>
                 </div>
             </li>

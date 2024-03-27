09ecc4ac74a142e84ad279a469fd344309891a4d
NayemHassan <nayem15-12498@diu.edu.bd>
2024-03-27 10:58:50 +0600

    customer section work

--- resources/views/body/js.blade.php
+++ resources/views/body/js.blade.php
@@ -83,5 +83,6 @@
             @endif
           </script>
     <script src="{{ asset('assets/js/codedeletesweet.js') }}"></script>
+    
     <script src="{{ asset('assets/js/myvalidate.min.js') }}"></script>
 <!-- End custom js for this page -->

1bda649888905530a81b781320c5404283e813bd
NayemHassan <nayem15-12498@diu.edu.bd>
2024-03-30 13:12:09 +0600

    product

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -19,6 +19,7 @@ class SubCategoryController extends Controller
     public function store(Request $request){
      $validator = Validator::make($request->all(), [
             'name' => 'required|max:255',
+            'category_id' => 'required|max:255',
         ]);
 
         if ($validator->passes()) {
@@ -50,6 +51,86 @@ class SubCategoryController extends Controller
         return response()->json([
             "status" => 200,
             "data" => $subcategories
+        ]);
+    }//
+    public function edit($id)
+    {
+        $subcategory = SubCategory::findOrFail($id);
+        $categories = Category::get();
+        if ($subcategory) {
+            return response()->json([
+                'status' => 200,
+                'subcategory' => $subcategory
+            ]);
+        } else {
+            return response()->json([
+                'status' => 500,
+                'message' => "Data Not Found"
+            ]);
+        }
+    }//
+    public function update(Request $request, $id)
+    {
+        // dd($request->all());
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'category_id' => 'required',
+        ]);
+        if ($validator->passes()) {
+            $subcategory = SubCategory::findOrFail($id);
+            $subcategory->name =  $request->name;
+            $subcategory->slug = Str::slug($request->name);
+            $subcategory->category_id =  $request->category_id;
+            if ($request->image) {
+                $imageName = rand() . '.' . $request->image->extension();
+                $request->image->move(public_path('uploads/subcategory'), $imageName);
+                if ($subcategory->image) {
+                    $previousImagePath = public_path('uploads/subcategory') . $subcategory->image;
+                    if (file_exists($previousImagePath)) {
+                        unlink($previousImagePath);
+                    }
+                }
+                $subcategory->image = $imageName;
+            }
+
+            $subcategory->save();
+            return response()->json([
+                'status' => 200,
+                'message' => 'sub Category Update Successfully',
+            ]);
+        } else {
+            return response()->json([
+                'status' => '500',
+                'error' => $validator->messages()
+            ]);
+        }
+    }//
+    public function destroy($id)
+    {
+        $subcategory = SubCategory::findOrFail($id);
+        if ($subcategory->image) {
+            $previousImagePath = public_path('uploads/subcategory/') . $subcategory->image;
+            if (file_exists($previousImagePath)) {
+                unlink($previousImagePath);
+            }
+        }
+        $subcategory->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Sub Category Deleted Successfully',
+        ]);
+    }//
+    public function status($id)
+    {
+        $subcategory = SubCategory::findOrFail($id);
+        $newStatus = $subcategory->status == 0 ? 1 : 0;
+        $subcategory->update([
+            'status' => $newStatus
+        ]);
+        return response()->json([
+            'status' => 200,
+            'newStatus' => $newStatus,
+            'message' => 'Status Changed Successfully',
         ]);
     }
 }

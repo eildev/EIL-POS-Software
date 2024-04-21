e0b7afb3e3dc34fb38682fef61e2bc881a572933
majid0177 <majid.bd905@gmail.com>
2024-04-21 16:08:17 +0600

    majid

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -28,23 +28,17 @@
 
         </div>
     </div>
-
     @include('body.js')
-
 </body>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
-    const flexSwitchCheckDefault = document.querySelector('.flexSwitchCheckDefault');
-    const form = document.getElementById('darkModeForm');
-
-    if (flexSwitchCheckDefault && form) {
-        flexSwitchCheckDefault.addEventListener('change', function() {
-
+        const flexSwitchCheckDefault = document.querySelector('.flexSwitchCheckDefault');
+        const form = document.getElementById('darkModeForm');
+        if (flexSwitchCheckDefault && form) {
+            flexSwitchCheckDefault.addEventListener('change', function() {
                 form.submit();
-        });
-    } else {
-        console.error('Element with ID #flexSwitchCheckDefault or form #darkModeForm not found.');
-    }
-});
+            });
+        }
+    });
 </script>
 </html>

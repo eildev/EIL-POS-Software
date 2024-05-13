c268475ac0e0f5c042dd59230bd19c7a13787482
NayemHassan <nayem15-12498@diu.edu.bd>
2024-05-13 12:15:02 +0600

    barcode genarator

--- resources/views/pos/products/product/product-show.blade.php
+++ resources/views/pos/products/product/product-show.blade.php
@@ -70,19 +70,22 @@
 
 
             <!-- Modal -->
-            <div class="modal fade" id="exampleModal{{$product->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade modal-lg" id="exampleModal{{$product->id}}"  tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body text-center">
 
                         @for($i = 0; $i < $product->stock; $i++)
-                        {!! DNS1D::getBarcodeHTML($product->barcode, 'PHARMA') !!}
-                        <p>P - {{$product->barcode}}</p>
-                    @endfor
+                        <div class="barcode-container">
+                            {!! DNS1D::getBarcodeHTML($product->barcode, 'PHARMA') !!}
+                            <span style="">{{$product->barcode}}</span><br>
+                            <span>{{ $product->name ?? '' }} </span><br>
+                            <span class="bold">{{ $product->price ?? 0 }}TK</span>
+                        </div>
+                        @endfor
                     </div>
                     <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -102,4 +105,12 @@
             </div>
         </div>
     </div>
+    <style>
+        .barcode-container {
+            text-align: center;
+            display: inline-block;
+            border: 2px solid #e9ecef;
+            padding: 10px;
+        }
+    </style>
 @endsection

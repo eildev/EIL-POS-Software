9383a2ddba833067d5387dcb3116767986e68d91
kishormahmud9 <topfazil999@gmail.com>
2024-05-15 11:56:16 +0600

    Add Chart on Dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -98,12 +98,41 @@
         //Month Sale Profit
         $monthSaleProfitAmount = $totalInvoiceMonthSum->sum('profit');
 
-        $salesByDay = [];
+        $salesByDayCount = [];
         for ($i = 6; $i >= 0; $i--) {
             $date = now()->subDays($i)->toDateString();
             $salesCount = App\Models\Sale::whereDate('sale_date', $date)->count();
-            $salesByDay[$date] = $salesCount;
+            $salesByDayCount[$date] = $salesCount;
         }
+
+        $salesByDay = [];
+        $salesProfitByDay = [];
+        $purchaseByDay = [];
+
+        for ($i = 6; $i >= 0; $i--) {
+            $date = now()->subDays($i)->toDateString();
+            $dailySales = App\Models\Sale::whereDate('sale_date', $date)->sum('receivable');
+            $dailyProfit = App\Models\Sale::whereDate('sale_date', $date)->sum('profit');
+            $dailyPurchase = App\Models\Purchase::whereDate('purchse_date', $date)->sum('grand_total');
+
+            $salesByDay[$date] = $dailySales;
+            $salesProfitByDay[$date] = $dailyProfit;
+            $purchaseByDay[$date] = $dailyPurchase;
+        }
+
+        // Fetching the latest 5 bank records
+        $banks = App\Models\Bank::take(5)->get();
+
+        // Array to store total transaction amounts for each bank
+        $totalTransactionAmounts = [];
+
+        // Loop through each bank to calculate total transaction amount
+        foreach ($banks as $bank) {
+            $totalTransactionAmount = App\Models\AccountTransaction::where('account_id', $bank->id)->sum('balance');
+            // array_push(floatval($totalTransactionAmounts), floatval($totalTransactionAmount));
+            array_push($totalTransactionAmounts, floatval($totalTransactionAmount));
+        }
+        // dd($totalTransactionAmounts);
     @endphp
 
 
@@ -339,60 +368,11 @@
 
 
     {{-- //////Revenew Chart Start /////// --}}
-    {{-- <div class="row my-3">
-        <div class="col-12 col-xl-12 grid-margin stretch-card">
-            <div class="card overflow-hidden">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-baseline mb-4 mb-md-3">
-                        <h6 class="card-title mb-0">Revenue</h6>
-                        <div class="dropdown">
-                            <a type="button" id="dropdownMenuButton3" data-bs-toggle="dropdown" aria-haspopup="true"
-                                aria-expanded="false">
-                                <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton3">
-                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                        data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
-                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                        data-feather="edit-2" class="icon-sm me-2"></i> <span
-                                        class="">Edit</span></a>
-                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                        data-feather="trash" class="icon-sm me-2"></i> <span
-                                        class="">Delete</span></a>
-                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                        data-feather="printer" class="icon-sm me-2"></i> <span
-                                        class="">Print</span></a>
-                                <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i
-                                        data-feather="download" class="icon-sm me-2"></i> <span
-                                        class="">Download</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row align-items-start">
-                        <div class="col-md-7">
-                            <p class="text-muted tx-13 mb-3 mb-md-0">Revenue is the income that a business has from its
-                                normal business activities, usually from the sale of goods and services to customers.</p>
-                        </div>
-                        <div class="col-md-5 d-flex justify-content-md-end">
-                            <div class="btn-group mb-3 mb-md-0" role="group" aria-label="Basic example">
-                                <button type="button" class="btn btn-outline-primary">Today</button>
-                                <button type="button" class="btn btn-outline-primary d-none d-md-block">Week</button>
-                                <button type="button" class="btn btn-primary">Month</button>
-                                <button type="button" class="btn btn-outline-primary">Year</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="revenueChart"></div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
     <div class="row">
         <div class="col-xl-6 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    <h6 class="card-title">Daily Profit</h6>
+                    <h6 class="card-title">Weekly Profit</h6>
                     <div id="apexLine1"></div>
                 </div>
             </div>
@@ -406,9 +386,6 @@
             </div>
         </div>
     </div>
-
-    {{-- //////Revenew Chart Start /////// --}}
-
     <script>
         $(document).ready(function() {
             var colors = {
@@ -460,7 +437,7 @@
                 series: [{
                     name: 'sales',
                     data: [
-                        @foreach ($salesByDay as $date => $salesCount)
+                        @foreach ($salesByDayCount as $date => $salesCount)
                             {{ $salesCount }},
                         @endforeach
                     ]
@@ -468,7 +445,7 @@
                 xaxis: {
                     type: 'datetime',
                     categories: [
-                        @foreach ($salesByDay as $date => $salesCount)
+                        @foreach ($salesByDayCount as $date => $salesCount)
                             '{{ $date }}',
                         @endforeach
                     ],
@@ -536,21 +513,37 @@
                     }
                 },
                 series: [{
-                        name: "Total Sale",
-                        data: [45, 52, 38, 45]
+                        name: "Weekly Sale",
+                        data: [
+                            @foreach ($salesByDay as $date => $dailySales)
+                                {{ $dailySales }},
+                            @endforeach
+                        ]
                     },
                     {
-                        name: "Profit",
-                        data: [12, 42, 68, 33]
+                        name: "Weekly Profit",
+                        data: [
+                            @foreach ($salesProfitByDay as $date => $dailyProfit)
+                                {{ $dailyProfit }},
+                            @endforeach
+                        ]
                     },
                     {
-                        name: "Purchase",
-                        data: [8, 32, 48, 53]
+                        name: "Weekly Purchase",
+                        data: [
+                            @foreach ($purchaseByDay as $date => $dailyPurchase)
+                                {{ $dailyPurchase }},
+                            @endforeach
+                        ]
                     }
                 ],
                 xaxis: {
                     type: "datetime",
-                    categories: ["2015", "2016", "2017", "2018"],
+                    categories: [
+                        @foreach ($salesByDay as $date => $salesCount)
+                            '{{ $date }}',
+                        @endforeach
+                    ],
                     lines: {
                         show: true
                     },
@@ -584,6 +577,10 @@
             apexLineChart.render();
         });
     </script>
+    {{-- //////Revenew Chart Start /////// --}}
+
+
+
 
 
     {{-- /////Current Month Summary/// --}}
@@ -807,6 +804,119 @@
         </div>
     </div> <!-- row -->
     {{-- /////EndCurrent Month Summary/// --}}
+
+
+    {{-- /// pie chart start /// --}}
+    {{-- <div class="row">
+        <div class="col-xl-6 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="card-title">Pie chart</h6>
+                    <div id="apexPie"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-6 stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h6 class="card-title">Banking Details</h6>
+                    <div id="apexRadialBar1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            var colors = {
+                primary: "#6571ff",
+                secondary: "#7987a1",
+                success: "#05a34a",
+                info: "#66d1d1",
+                warning: "#fbbc06",
+                danger: "#ff3366",
+                light: "#e9ecef",
+                dark: "#060c17",
+                muted: "#7987a1",
+                gridBorder: "rgba(77, 138, 240, .15)",
+                bodyColor: "#b8c3d9",
+                cardBg: "#0c1427"
+            }
+
+            var fontFamily = "'Roboto', Helvetica, sans-serif"
+            var options = {
+                chart: {
+                    height: 300,
+                    type: "radialBar",
+                    parentHeightOffset: 0,
+                    foreColor: colors.bodyColor,
+                    background: colors.cardBg,
+                    toolbar: {
+                        show: false
+                    },
+                },
+                theme: {
+                    mode: 'dark'
+                },
+                tooltip: {
+                    theme: 'dark'
+                },
+                colors: [colors.primary, colors.warning, colors.danger, colors.info],
+                fill: {
+
+                },
+                grid: {
+                    padding: {
+                        top: 10
+                    }
+                },
+                plotOptions: {
+                    radialBar: {
+                        dataLabels: {
+                            total: {
+                                show: true,
+                                label: 'TOTAL',
+                                fontSize: '14px',
+                                fontFamily: fontFamily,
+                            }
+                        },
+                        track: {
+                            background: colors.gridBorder,
+                            strokeWidth: '100%',
+                            opacity: 1,
+                            margin: 5,
+                        },
+                    }
+                },
+                series: [
+                    @foreach ($totalTransactionAmounts as $element)
+                        {{ $element }},
+                    @endforeach
+                ],
+                labels: [
+                    @foreach ($banks as $bank)
+                        '{{ $bank->name }}',
+                    @endforeach
+                ],
+                legend: {
+                    show: true,
+                    position: "top",
+                    horizontalAlign: 'center',
+                    fontFamily: fontFamily,
+                    itemMargin: {
+                        horizontal: 8,
+                        vertical: 0
+                    },
+                },
+            };
+
+            var chart = new ApexCharts(document.querySelector("#apexRadialBar1"), options);
+            chart.render();
+            var chartAreaBounds = chart.w.globals.dom.baseEl.querySelector('.apexcharts-inner')
+                .getBoundingClientRect();
+        });
+    </script> --}}
+    {{-- /// pie chart end /// --}}
     {{-- //////Start Total Summary /////// --}}
     <div class="row">
         <div class="col-12 col-xl-12 stretch-card">

3fb035a1e07d3fc2fbb957e8a1b6e87e31fb6006
NayemHassan <nayem15-12498@diu.edu.bd>
2024-05-15 16:36:47 +0600

    expense image

--- resources/views/pos/expense/edit_expense.blade.php
+++ resources/views/pos/expense/edit_expense.blade.php
@@ -62,8 +62,29 @@
                             <div class="col-sm-6">
                                 <div class="mb-3 form-valid-groups">
                                     <label class="form-label">Date<span class="text-danger">*</span></label>
-                                    <input type="date" name="expense_date" value="{{ $expense->expense_date }}"
-                                        class="form-control" placeholder="Enter Date">
+                                    
+                                        <div class="input-group flatpickr" id="flatpickr-date">
+                                            <input type="text"name="expense_date" value="{{ $expense->expense_date }}"
+                                                class="form-control @error('expense_date') is-invalid @enderror flatpickr-input"
+                                                data-input="" readonly="readonly"
+                                                placeholder="Select Expense Date">
+                                            <span class="input-group-text input-group-addon"
+                                                data-toggle=""><svg xmlns="http://www.w3.org/2000/svg"
+                                                    width="24" height="24" viewBox="0 0 24 24"
+                                                    fill="none" stroke="currentColor" stroke-width="2"
+                                                    stroke-linecap="round" stroke-linejoin="round"
+                                                    class="feather feather-calendar">
+                                                    <rect x="3" y="4" width="18" height="18"
+                                                        rx="2" ry="2">
+                                                    </rect>
+                                                    <line x1="16" y1="2" x2="16"
+                                                        y2="6"></line>
+                                                    <line x1="8" y1="2" x2="8"
+                                                        y2="6"></line>
+                                                    <line x1="3" y1="10" x2="21"
+                                                        y2="10"></line>
+                                                </svg></span>
+                                        </div>
                                 </div>
                             </div>
                             <div class="col-sm-6">

47a67532069d90fd62ab7e158ea58e3927c8aa51
NayemHassan <nayem15-12498@diu.edu.bd>
2024-05-15 11:29:28 +0600

    barcode Print

--- resources/views/pos/products/product/product-barcode.blade.php
+++ resources/views/pos/products/product/product-barcode.blade.php
@@ -1,13 +1,17 @@
 @extends('master')
 @section('admin')
 <div>
-    <button class="btn btn-info text-center print" onClick="window.print();">Print</button><br><br><br>
+    <div class="btn-print">
+        <button class="btn btn-info text-center" onClick="window.print();">Print</button> </br></br>
+    </div>
+
     <div class="row">
         @for($i = 0; $i < $product->stock; $i++)
-        <div class="col-md-4">
+        <div class="col-md-4 p-0 printable">
         <div class="barcode-container">
             <span class="dblock">
-            {!! DNS1D::getBarcodeHTML($product->barcode, 'PHARMA') !!}</span><br>
+                <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($product->barcode, 'C39',1,30) }}" alt="Barcode"> </br>
+            {{-- {!! DNS1D::getBarcodeHTML($product->barcode, 'PHARMA') !!}</span><br> --}}
             <span style="">{{$product->barcode}}</span><br>
             <span>{{ $product->name ?? '' }} </span><br>
             <span class="bold">{{ $product->price ?? 0 }}TK</span>
@@ -24,9 +28,6 @@
         border: 1px solid #e9ecef;
         padding: 10px;
     }
-    .dblock{
-        display: inline-block;
-    }
     @media print {
         .header{
             display: none!important;
@@ -39,16 +40,29 @@
             left: 0;
             top: 0;
         }
-        .header, .nav, .sidebar,.navbar {
+        .header,.nav, .sidebar,.navbar {
             display: none;
         }
-        .navbar,.print,.footer{
+        .navbar{
             margin-top: 0!important;
             display: none!important;
         }
-        .footer {
+        .footer{
+            display: none;
+        }
+        .btn-print{
             display: none!important;
         }
+        .printable {
+                display: block !important;
+
+            }
+
+            /* Ensure the printable element takes the full width of the page */
+            .printable {
+                width: 100%;
+            }
+
 }
 </style>
 

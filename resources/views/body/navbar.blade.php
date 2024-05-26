9c3b2bc634babcd01bc73d5a70c1a19c3af3f33b
majid0177 <majid.bd905@gmail.com>
2024-05-26 09:28:53 +0600

    test

--- resources/views/body/navbar.blade.php
+++ resources/views/body/navbar.blade.php
@@ -5,9 +5,9 @@
      <div class="navbar-content">
          <form class="search-form">
              <div class="input-group">
-                 <div class="input-group-text">
+                 {{-- <div class="input-group-text">
                      <i data-feather="search"></i>
-                 </div>
+                 </div> --}}
                  <input type="text" class="form-control" id="navbarForm" placeholder="Search here...">
              </div>
          </form>

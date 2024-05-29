a3f1a0c9cdb753656af4f992d6e8cc519a363ebc
NayemHassan <nayem15-12498@diu.edu.bd>
2024-05-29 10:17:41 +0600

    mobile dashboard change

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -193,31 +193,29 @@
 
             <div class="row flex-grow-1">
                 <h3 class="mb-3">Today Summary</h3>
-                <div class="col-md-3 grid-margin stretch-card">
+                <div class="col-md-3 col-xl-3 col-6 new-margin grid-margin stretch-card">
                     <div class="card" style="">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-baseline">
                                 {{-- <h6 class="card-title mb-0">Invoice</h6> --}}
                             </div>
-                            <div class="row">
-
-                                <div class=" col-md-4">
+                            <div class="row" >
+                                <div class=" col-md-4 col-5 ">
                                     <img src="uploads/dashboard/Artboard4@300x-100.jpg" height="50px" width="50px" alt="Image" style="border-radius:5px">
                                 </div>
-                                <div class="col-md-8">
+                                <div class="col-md-8 col-7 ">
                                     {{-- <h3 class="mb-2"> {{ $totalInvoiceToday }}
-
                                         <span style="font-size: 15px;">
                                         (৳ {{ $saleProfitAmount }})</span>
                                 </h3> --}}
-                                <h3>{{ $saleProfitAmount }}</h3>
-                                <h6 class="mb-0">Profit</h6>
+                                <h3 class="responsive-text mar-1">{{ $saleProfitAmount }}</h3>
+                                <h6 class="text-1 mb-0">Profit</h6>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 grid-margin stretch-card">
+                <div class="col-md-3 col-xl-3 col-6 grid-margin stretch-card">
                     <div class="card" style="">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-baseline">
@@ -225,56 +223,56 @@
 
                             </div>
                             <div class="row">
-                                <div class="col-md-4">
+                                <div class="col-md-4 col-5 ">
                                     <img src="uploads/dashboard/Artboard3@300x-100.jpg" height="50px" width="50px" alt="Image" style="border-radius:5px">
                                 </div>
-                                <div class="col-md-8 ">
+                                <div class="col-md-8 col-7 ">
                                     {{-- <h3 class="mb-2">{{ $todayPurchaseToday }}<span style="font-size: 15px;">
                                             (৳ {{ $todayExpenseAmount }})</span></h3> --}}
-                                            <h3>{{ $todayExpenseAmount }}</h3>
-                                 <h6 class=" mb-0">Expense</h6>
+                                    <h3 class="responsive-text mar-1">{{ $todayExpenseAmount }}</h3>
+                                 <h6 class="text-1  mb-0">Expense</h6>
                                  {{-- <h6 class=" mb-0">Total Purchase</h6> --}}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 grid-margin stretch-card">
+                <div class="col-md-3  col-xl-3 col-6 new-margin grid-margin stretch-card">
                     <div class="card" style="">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-baseline">
                                 {{-- <h6 class="card-title mb-0">invoice product</h6> --}}
                             </div>
                             <div class="row">
-                                <div class=" col-md-4">
+                                <div class=" col-md-4  col-5">
                                     <img src="uploads/dashboard/Artboard1@300x-100.jpg" height="50px" width="50px" alt="Image" style="border-radius:5px">
                                 </div>
-                                <div class="col-md-8">
+                                <div class="col-md-8  col-7">
                                     {{-- <h3 class="mb-2">{{ $todayInvoiceProductTotal }} <span
                                             style="font-size: 15px;">( ৳
                                             {{ $todayInvoiceProductAmount }})</span></h3> --}}
-                                            <h3>{{ $todayInvoiceProductAmount }}</h3>
-                                            <h6 class=" mb-0">Invoice</h6>
+                                            <h3 class="responsive-text mar-1">{{ $todayInvoiceProductAmount }}</h3>
+                                            <h6 class="text-1 mb-0">Invoice</h6>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 grid-margin stretch-card">
+                <div class="col-md-3  col-xl-3 col-6  grid-margin stretch-card">
                     <div class="card" style="">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-baseline">
                                 {{-- <h6 class="card-title mb-0">invoice amount</h6> --}}
                             </div>
                             <div class="row">
-                                <div class=" col-md-4">
-                                    <img src="uploads/dashboard/Artboard5@300x-100.jpg" height="50px" width="50px" alt="Image" style="border-radius:5px">
+                                <div class=" col-md-4  col-5">
+                                    <img src="uploads/dashboard/Artboard5@300x-100.jpg" height="auto" width="50px" alt="Image" style="border-radius:5px">
                                 </div>
-                                <div class="col-md-8">
+                                <div class="col-md-8  col-7">
                                     {{-- <h3 class="mb-2"> ৳ {{ $todayInvoiceAmount }}<span
                                             style="font-size: 15px;"> (৳ {{ $todayProfit }})</span></h3> --}}
-                                            <h3>{{$todayPurchaseAmont }}</h3>
-                                    <h6 class=" mb-0">Purchase</h6>
+                                            <h3 class="responsive-text mar-1">{{$todayPurchaseAmont }}</h3>
+                                    <h6 class="text-1 mb-0">Purchase</h6>
                                 </div>
                             </div>
                         </div>
@@ -369,7 +367,29 @@
     </div> <!-- row -->
     {{-- //////End Today /////// --}}
 
+<style>
+    @media (max-width: 768px) {
+    .responsive-text {
+        font-size: 1rem; /* Adjust as needed for medium screens */
 
+    }
+    .grid-margin {
+        margin-bottom: .7rem;
+        padding-right: 6px;
+        padding-left: 6px;
+    }
+    .new-margin{
+        padding-right: 0px!important;
+    }
+    .text-1{
+        font-size: 13px
+    }
+    .mar-1{
+        margin-bottom: 4px
+    }
+
+}
+</style>
     {{-- //////Revenew Chart Start /////// --}}
     <div class="row">
         <div class="col-xl-6 grid-margin stretch-card">
@@ -819,7 +839,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-6 stretch-card">
+        <div class="col-xl-6 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
                     <h6 class="card-title">Banking Details</h6>
